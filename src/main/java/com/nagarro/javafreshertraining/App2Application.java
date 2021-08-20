da87e5f82f76159e27b5b0383585f0543f1d914e
groverv04 <grovervaishally17@gmail.com>
2021-08-20 17:16:22 +0530

    APIs

---
+++ src/main/java/com/nagarro/javafreshertraining/App2Application.java
@@ -0,0 +1,21 @@
+package com.nagarro.javafreshertraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author vaishally
+ *
+ */
+@SpringBootApplication
+public class App2Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(App2Application.class, args);
+	}
+
+}
